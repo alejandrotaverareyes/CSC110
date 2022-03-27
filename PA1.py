f452947589b7e91492b4999f1df4b7993e673319
Alejandro Tavera-Reyes <Alejandro@Marcos-MacBook-Air.local>
2022-03-27 15:40:17 -0700

    Changed pa1

--- PA1.py
+++ PA1.py
@@ -1,6 +1,14 @@
-def numberOfLeap(year1,year2):
-    count = 0
-    for i in range(year1,year2+1):
-        if leapYear(i):
-            count += 1
-    return 
+width = input('Enter TIE width: \n')
+arms = '/' * int(width)
+arms2 = "\\" * int(width)
+Sarms = ' ' * int(width) #shadow arms
+
+print('|[' + Sarms + '         ' + Sarms + ']|')       
+print('|[' + Sarms + '         ' + Sarms + ']|')
+print('|[' + Sarms + ' ' + '/=---=\\'+ ' '+  Sarms + ']|') #7
+print('|[' + Sarms + '/==---==\\' + Sarms +  ']|' )#9
+print('|[' + arms +  '|== X ==|' + arms2 +    ']|') #9
+print('|[' + Sarms + '\==---==/' +  Sarms  +']|')  #7
+print('|[' + Sarms + ' ' + '\=---=/' + ' ' + Sarms  +']|'  )
+print('|[' + Sarms + '         ' + Sarms + ']|')
+print('|[' + Sarms + '         ' + Sarms + ']|')
